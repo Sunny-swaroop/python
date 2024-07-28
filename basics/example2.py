6686bcf6fd6fb93e479f5209ecaf30a41dac82b7
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-28 10:27:23 +0000

    basic examples

---
+++ basics/example2.py
@@ -0,0 +1,16 @@
+#2nd example
+#addition
+#subtraction
+#multiplication
+#divison
+a=10
+b=20
+c=a+b
+d=a-b
+e=a*b
+f=a//b
+print(c)
+print(d)
+print(e)
+print(f)
+
---
+++ basics/example4.py
@@ -0,0 +1,13 @@
+#4th example
+#converting into data types
+a=10
+print(a)
+print(type(a))
+a=float(a)
+print(type(a))
+b=11.25
+print(b)
+print(type(b))
+b=int(b)
+print(type(b))
+
