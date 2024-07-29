8e5987ccd349c56909b9ac4f5cc61fe593eb22d0
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-29 07:23:31 +0000

    dict example

---
+++ datatypes/non-sequential/dictionary.py
@@ -0,0 +1,8 @@
+#dictionary data type
+dict1={}#by default its the values in this variable are store in dictionary
+dict2={'name' : 'sunny'}#in dict we have key values and items
+#name is key value
+#sunny is item
+print(dict2)
+print(type(dict2))
+
