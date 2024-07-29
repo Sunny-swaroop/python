b8c0fea20d84dd20ff25d752ee1138629d9f3268
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-29 14:38:24 +0000

    elif example

---
+++ control_statements/conditions/elif.py
@@ -0,0 +1,10 @@
+#elif statements
+#elif statements works when the if statement is wrong
+num1=int(input("enter a number:"))
+num2=int(input("enter 2nd number :"))
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num1 < num2:
+    print(f"{num1} is less than {num2}")
+
+
