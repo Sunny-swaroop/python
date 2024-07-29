8da582ec47cd711112b25246eb13136acfddcc9c
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-29 11:33:32 +0000

    examples

---
+++ operators/logical/and.py
@@ -0,0 +1,16 @@
+#logical operators
+#and operator
+a=int(input("enter a number:"))
+b=int(input("enter second number:"))
+c=int(input("enter the third number:"))
+if a>b and a>c: #and operator and also if condition to check the statement is true or false
+    print(f"{a} is greater number")
+elif b>c:
+    print(f"{b} is greater number")
+else:
+    print(f"{c} is greater number")
+
+#this is also an example for maximum number from 3 numbers
+
+
+
