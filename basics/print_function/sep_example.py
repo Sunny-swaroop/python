ac3707c0e00c6a1109d6703bacbbefeaeab2edea
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-30 06:45:40 +0000

    using sep and example

---
+++ basics/print_function/sep_example.py
@@ -0,0 +1,12 @@
+#"sep"
+#function to specify the separator between the values that are being printed.
+#By default, print() separates the values with a space, but you can change this behavior using the sep parameter.
+print("apple""banana""cherry")
+       #now it will print (applebananacherry) with no spaces in between them
+print("apple","banana","cherry")
+    #now we are giving comma in between them, it will print space between them
+print("apple","banana","cherry",sep=",")
+    #now it will print comma between them (apple, banana,cherry)
+
+
+
