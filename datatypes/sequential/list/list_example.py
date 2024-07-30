92d8779d9818e27d3ed887e5ff097a7a4be43c9c
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-30 07:04:22 +0000

    examples

---
+++ datatypes/sequential/list/list_example.py
@@ -0,0 +1,15 @@
+#list data type
+#list an collection of homogenous and non-homogenous elements
+list1=[1,2,3,4,5,6,7,8,9,10]#to define list we have to use '[]'
+print(list1)
+print(type(list1))
+
+
+
+#from the keyboard entering the list using for loop
+list2=[]
+number=int(input("enter the numbers :"))
+for i in range(1,number+1):
+    list2.append(i)
+print(list2)
+
