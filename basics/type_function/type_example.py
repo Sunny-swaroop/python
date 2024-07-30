184b4f735a2239a3f4819b49129af2aa317d493a
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-30 06:28:43 +0000

    with example

---
+++ basics/type_function/type_example.py
@@ -0,0 +1,21 @@
+
+#type function
+#it is pre defined function
+#The type() function is used to get the type of an object.
+a=10
+print(a)
+print(type(a))
+#here it will show the data type of "a"-----"integer"
+b=5.5
+print(b)
+print(type(b))
+#it is in float
+c="sunny"
+print(c)
+print(type(c))
+#it is in "string"
+d=True
+print(d)
+print(type(d))
+#it is in boolean
+
