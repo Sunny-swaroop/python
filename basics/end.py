f8e333669b5b50c4429da7cbdabf8cfaec6cfecc
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-30 05:27:34 +0000

    remove this file

--- basics/end.py
+++
@@ -1,9 +0,0 @@
-#basics
-#"end" 
-print("hello")
-print("Good morning")
-#these two statements will print in separate lines
-#now we will  print in a single line by using "end"
-print("hello",end="\t")#here "\t" will give tab space
-print("Good morining")
-
