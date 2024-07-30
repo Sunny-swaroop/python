4b2ab6fd3b44ff37358b3bd384673292a3d25095
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-30 05:47:58 +0000

    input function examples

---
+++ basics/input_function/input_example1.py
@@ -0,0 +1,9 @@
+#input function
+#it is pre-defined function
+#by using input function we can directly assign the values from the user
+a=input("enter the number :")#by default it will store in string
+print(type(a))
+b=int(input("enter the number :"))
+       #now it will store in integer 
+print(type(b))
+
