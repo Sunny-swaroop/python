8e9e17e15c875053673aee78d78a8dc43319b24f
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-30 08:12:48 +0000

    elif example

---
+++ control_statements/conditions/elif.py
@@ -0,0 +1,28 @@
+#if -elif-else:
+#example calculator
+#elif condition is executed when if condition is false
+num1=int(input("enter the first number :"))
+num2=int(input("enter the second number :"))
+print("choose your option :")
+a=print("a.Addition")
+b=print("b.Subtraction")
+c=print("c.Multiplication")
+d=print("d.divison")
+result=input("enter your option:")
+if result=='a':
+    result=num1+num2
+    print(f"the sum of {num1} + {num2} = {result}")
+elif result=='b':
+    result=num1-num2
+    print(f"the subtraction of {num1} - {num2} = {result}")
+elif result=='c':
+    result=num1*num2
+    print(f"the multiplication of {num1} * {num2} = {result}")
+elif result == 'd':
+    result=num1/num2
+    print(f"the division of {num1} / {num2} = {result}")
+else:
+    print("you have choosed the invalid option")
+
+
+
