680b796517e1f981628559f022bde8a2fda29811
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-31 17:47:22 +0000

    employe salary checking

--- control_statements/conditions/if/if_examples/employe_salary.py
+++ control_statements/conditions/if/if_examples/employe_salary.py
@@ -23,15 +23,14 @@
       print(f"the DA is {da}")
       income_tax=(basic_salary*(15/100))
       print(f"the income tax is {income_tax}")
- elif basic_salary>100000:
+ else:
+     basic_salary>100000:
       hra=(basic_salary*(40/100))
       print(f"the HRA is {hra}")
       da=(basic_salary*(25/100))
       print(f"the DA is {da}")
       income_tax=(basic_salary*(20/100))
       print(f"the income tax is {income_tax}")
- else:
-      print(f"{name},{employe_id} please check your details")
  gross_salary=basic_salary+hra+da
  net_salary=gross_salary-income_tax
  print(f"the gross salary {name} is {gross_salary}")
