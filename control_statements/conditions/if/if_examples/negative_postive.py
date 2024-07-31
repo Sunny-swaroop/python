42d0610f579f550adc0a0bb022f208328e8ca933
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-31 09:47:21 +0000

    checking +ve or -ve

---
+++ control_statements/conditions/if/if_examples/negative_postive.py
@@ -0,0 +1,9 @@
+#checking postive or negative numbers
+num=eval(input("Enter a number :"))
+if num < 0:
+    print(f"the number {num} is Negative")
+elif num==0:
+    print(f"the number {num} is Intial number")
+else:
+    print(f"the number {num} is postive")
+        
