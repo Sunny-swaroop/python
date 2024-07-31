c9699297a4c4f96e026946d0e168a63bd8a806c7
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-31 15:56:49 +0000

    even or odd example

--- control_statements/conditions/if/if_examples/even_odd.py
+++ control_statements/conditions/if/if_examples/even_odd.py
@@ -1,8 +1,6 @@
 #even or odd checking
 num1=int(input("enter the number:"))
-if num1 > 0:
-    result=num1%2
-    if result==0:
+if num1%2==0:
         print(f"The {num1} is a even number")
     else:
         print(f"The {num1} is a odd number")
