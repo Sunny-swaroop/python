db9e1b70812d57db052c3836bab6600390efeac2
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-31 16:32:20 +0000

    rainbow colours

--- control_statements/conditions/if/if_examples/rainbow.py
+++ control_statements/conditions/if/if_examples/rainbow.py
@@ -1,25 +1,21 @@
 
 #rainbow clours
-num=int(input("enter the number from 1 to 7 :"))
-if num <= 0:
-    print("its invalid")
+print("the rainbow colours are V-voielt,I-indigo,B-blue,G-green,Y-yellow,O-orange,R-red")
+colour=input("choose your option to print the colour :")
+if colour=='v':
+    print("The clour RED")
+elif colour=='I':
+    print("the clour Indigo")
+elif colour=='B':
+    print("the colour Blue")
+elif colour=='G':
+    print("the colour is GREEN")
+elif colour=='Y':
+    print("the colour is YELLOW")
+elif colour=='O':
+    print("the colour is ORANGE")
+elif colour=='R':
+    print("the colour is RED")
 else:
-    print("choose your colours in Rainbow")
-    if num==1:
-        print("the colour is RED")
-    elif num==2:
-        print("the colour is ORANGE")
-    elif num==3:
-        print("the colour is YELLOW")
-    elif num==4:
-        print("the colour is GREEN")
-    elif num==5:
-        print("the colour is BLUE")
-    elif num==6:
-        print("the colour is INDIGO")
-    elif num==7:
-        print("the colour is VIOLET")
-    else:
-        print(" you are choosing invalid Rainbow colour")
-        
+    print("you have choosed invalid option")
 
