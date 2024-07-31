241ffcaad4b30b7b73f0c12b507c7c294f598f1f
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-31 17:19:58 +0000

    current bill checking

--- control_statements/conditions/if/if_examples/current-bill.py
+++ control_statements/conditions/if/if_examples/current-bill.py
@@ -10,26 +10,18 @@ else:
     if total_units <= 100:
         total_charge=2.50
         print(f"{meter_number} we are pricing {total_charge}rs for one unit")
-        total_amount=total_units*total_charge
-        print(f"the current bill of this month is {total_amount}rs")
     elif total_units <=150:
         total_charge=3.50
         print(f"{meter_number} we are pricing {total_charge}rs for one unit")
-        total_amount=total_units*total_charge
-        print(f"the current bill of this month is {total_amount}rs")
     elif total_units <=200:
         total_charge=4.50
         print(f"{meter_number} we are pricing {total_charge}rs for one unit")
-        total_amount=total_units*total_charge
-        print(f"the current bill of this month is {total_amount}rs")
     elif total_units <=300:
         total_charge=5.50
         print(f"{meter_number} we are pricing {total_charge}rs for one unit")
-        total_amount=total_units*total_charge
-        print(f"the current bill of this month is {total_amount}rs")
     else:
         total_charge=6.50
         print(f"{meter_number} we are pricing {total_charge}rs for one unit")
-        total_amount=total_units*total_charge
-        print(f"the current bill of this month is {total_amount}rs")
+    total_amount=total_units*total_charge
+    print(f"the current bill of this month is {total_amount}rs")
 
