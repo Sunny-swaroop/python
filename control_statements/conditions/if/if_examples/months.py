4e22ae987f16b77f353c1cc3030c5f963a1735ef
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-31 08:36:53 +0000

    checking months in a year

---
+++ control_statements/conditions/if/if_examples/months.py
@@ -0,0 +1,34 @@
+#checking months in a year
+num=int(input("choose a month in a year :"))
+if num <= 0:
+    print("its invalid")
+else:
+    if num==1:
+        print(f"you have choosed {num} the month is JANUARY")
+    elif num==2:
+        print(f"you have choosed {num} the month is FEBRUARY")
+    elif num==3:
+        print(f"you have choosed {num} the month is MARCH")
+    elif num==4:
+        print(f"you have choosed {num} the month is APIRL")
+    elif num==5:
+        print(f"you have choosed {num} the month is MAY")
+    elif num==6:
+        print(f"you have choosed {num} the month is JUNE")
+    elif num==7:
+        print(f"you have choosed {num} the month is JULY")
+    elif num==8:
+        print(f"you have choosed {num} the month is AUGUST")
+    elif num==9:
+        print(f"you have choosed {num} the month is SEPTEMBER")
+    elif num==10:
+        print(f"you have choosed {num} the month is OCTOBER")
+    elif num==11:
+        print(f"you have choosed {num} the month is NOVEMBER")
+    elif num==12:
+        print(f"you have choosed {num} the month is DECEMBER")
+    else:
+        print("you have choosed invalid option")
+        
+        
+
