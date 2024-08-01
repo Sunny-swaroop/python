fbbef136a4ae1c1f3e495f2af55d52c3688c2531
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-01 12:26:24 +0000

    example

---
+++ control_statements/loop/while/polindrum.py
@@ -0,0 +1,11 @@
+#polindrum number
+num=input("enter a number:")
+i=1
+n=1
+while i<=n:
+    if num==num[::-1]:
+        print(f"{num}is a palindrome")
+    else:
+        print(f"{num} not a palindrome")
+    i+=1
+
