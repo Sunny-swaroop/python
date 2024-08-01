65744ed90c79578d8eca1ce0c1463f20afbc302f
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-01 06:44:50 +0000

    while loop and its example

---
+++ control_statements/loop/while/while.py
@@ -0,0 +1,10 @@
+
+#while loop
+#while loop is used to execute some group of statements until some condition is false
+#example
+#printing 1 to 10 numbers
+num1=1
+while num1<=10:
+    print(num1)
+    num1+=1
+    
