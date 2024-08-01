18ab7d04f5305fe59314906810ab3a11e66d2f0b
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-01 12:55:06 +0000

    using for loop

---
+++ control_statements/loop/for/product.py
@@ -0,0 +1,10 @@
+#product of n number
+
+num=int(input("enter the number:"))
+s=1
+for i in range(1,num+1):
+    s=s*i
+print(f"the product of {num} is {s}")
+
+
+
---
+++ control_statements/loop/for/sum.py
@@ -0,0 +1,8 @@
+#sum of n number
+num=int(input("enter the number:"))
+s=0
+for i in range(num+1):
+    s=s+i
+print(f"the sum of {num} is {s}")
+
+
