bd14e53e155cc8f3bd68f8e2c947175ff779cbba
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-01 06:56:49 +0000

    examples

---
+++ control_statements/loop/while/printing_numbers_from_10_to_1.py
@@ -0,0 +1,7 @@
+#printing numbers from 10 to 1
+#using while loop
+num2=10
+while num2>0:
+    print(num2,end="\t")
+    num2-=1
+
---
+++ control_statements/loop/while/printing_numbers_from_1_to_10.py
@@ -0,0 +1,7 @@
+#printing numbers from 1 to 10
+#using while loop
+num1=1
+while num1<=10:
+    print(num1,end="\t")
+    num1+=1
+    
