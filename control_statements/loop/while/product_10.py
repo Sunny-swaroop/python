19d3207c4a2f6c7e0353b223da0cf1ceccaae482
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-01 07:14:44 +0000

    examples

---
+++ control_statements/loop/while/product_10.py
@@ -0,0 +1,10 @@
+#product of 1 to 10 numbers
+#using while loop
+num1=10
+i=1
+t=1
+while i<=num1:
+    t=t*i
+    i=i+1
+print(f"the product of 1 to 10 numbers is {t}")
+
---
+++ control_statements/loop/while/sum_of_1_to_10.py
@@ -0,0 +1,14 @@
+
+#sum of numbers from 1 to 10
+#using while loop
+num1=10
+i=1
+t=0
+while i<=num1:
+    t=t+i
+    i+=1
+print(f"the sum of 1 to 10 numbers is {t}")
+
+
+
+
