2bd33b66e886351b72bc847b4695191ed7375b30
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-01 06:37:40 +0000

    for and while loops

---
+++ control_statements/loop/for/for.py
@@ -0,0 +1,11 @@
+#for loop
+#for loop is used to execute some action for every element present in some sequence,
+#it may be string or collection.
+#example
+str1=input("enter any name:")  #if we enter "sunny" here 
+for i in s:
+    print(i)                      #output will be: s
+                                 #                 u
+                                 #                 n
+                                 #                 n
+                                 #                 y
