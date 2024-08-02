e09402b5e589313a1e4d41f2bc1f3e8949a193d2
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-02 06:28:43 +0000

    examples

---
+++ functions/even_odd.py
@@ -0,0 +1,11 @@
+#checking even or odd
+def even_odd():
+    num1=int(input("enter a number :"))
+    if num1%2==0:
+        print(f"the number {num1} is a even number")
+    else:
+        print(f"the number {num1} is a odd number")
+
+even_odd()
+
+
